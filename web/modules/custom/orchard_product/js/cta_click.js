e046290f1c0786a0b39e663d0b28d24226ae9ea4
Luis Correa <luiscarloscorreamartinez@gmail.com>
2025-06-06 08:44:20 -0500

    CTA Click js

--- web/modules/custom/orchard_product/js/cta_click.js
+++ web/modules/custom/orchard_product/js/cta_click.js
@@ -1,17 +1,18 @@
 (function (Drupal, once) {
-  Drupal.behaviors.orchardCtaClick = {
+  Drupal.behaviors.orchardCtaLinkClick = {
     attach(context, settings) {
-      once('orchard-cta', '.cta-button', context).forEach((button) => {
-        button.addEventListener('click', function () {
-          const productId = this.getAttribute('data-product-id');
+      once('cta-link', '.cta-link', context).forEach((link) => {
+        link.addEventListener('click', function (event) {
+          const productId = this.dataset.productId;
+
+          event.preventDefault();
+
           fetch(`/orchard-product/click?product_id=${productId}`)
-            .then(response => response.json())
-            .then(data => {
-              if (data.status === 'success') {
-                console.log('Click logged successfully');
-              } else {
-                console.error('Error logging click:', data.message);
-              }
+            .then(() => {
+              window.location.href = this.href;
+            })
+            .catch(() => {
+              window.location.href = this.href;
             });
         });
       });
